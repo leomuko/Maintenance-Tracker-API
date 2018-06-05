c7d0e8998f108efbe3a6275c018c586f791e7c68
leomuko <kezzieleo@gmail.com>
2018-06-05 19:11:30 +0300

    Modify models.py

--- app/models.py
+++ app/models.py
@@ -1,10 +1,10 @@
 import psycopg2
+import pprint
 
 
-class CreateTables:
+class USERS:
     def __init__(self):
         self.dbconnect = psycopg2.connect(database="postgres", user = "postgres", password = "test123", host = "127.0.0.1", port = "5432")
-        self.dbconnect.autocommit = True
         self.cursor = self.dbconnect.cursor()
     
 
@@ -12,27 +12,45 @@ class CreateTables:
     def create_user_table(self):
         user_table_create = "CREATE TABLE Users(User_Id serial PRIMARY KEY, FirstName varchar(100) NOT NULL, LastName varchar(100) NOT NULL,Email varchar(100) NOT NULL, password varchar(100) NOT NULL)"
         self.cursor.execute(user_table_create)
+        self.dbconnect.commit()
 
     def create_new_user(self,Fname,Lname,Email,Password):
-        new_user =   "INSERT INTO Users(FirstName, LastName, Email, password) VALUES(%s,%s,%s,%s)",(Fname, Lname, Email,Password)
+        new_user =   "INSERT INTO Users(FirstName, LastName, Email, password) VALUES('{}','{}','{}','{}')".format(Fname, Lname, Email,Password)
+        
         self.cursor.execute(new_user)
+        self.dbconnect.commit()
+
+    def login_user(self,email,password):
+        user = "SELECT * FROM Users WHERE Email = '{}' AND password = '{}' ".format(email,password)
+        self.cursor.execute(user)
+        self.dbconnect.commit()    
 
     
 
 class Requests():
     def __init__(self):
         self.dbconnect = psycopg2.connect(database="postgres", user = "postgres", password = "test123", host = "127.0.0.1", port = "5432")
-        self.dbconnect.autocommit = True
         self.cursor = self.dbconnect.cursor()
 
     def create_request_table(self):
         request_table_create = "CREATE TABLE Requests(Request_Id serial PRIMARY KEY, UserId INT REFERENCES Users(User_Id), RequestType varchar(100) NOT NULL, RequestDetails varchar(200) NOT NULL )"
         self.cursor.execute(request_table_create)
+        self.dbconnect.commit()
+
+    def view_all_requests(self):
+        requests = "SELECT * FROM Requests"
+        self.cursor.execute(requests)
+        self.dbconnect.commit()
+
+    def all_requests_for_specific_user(self,userId):
+        requests = "SELECT * FROM Requests WHERE UserId = '{}' ".format(userId)
+        self.cursor.execute(requests)
+        self.dbconnect.commit()          
     
     
-if __name__ == '__main__':
-    User_table = CreateTables()
-    Request_table = Requests() 
+'''if __name__ == '__main__':
+    User_table = USERS()
+    Request_table = Requests() '''
 
     
 
