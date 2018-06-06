72bb99b9e15d38a4c1f1b3b5f233231a17799929
leomuko <kezzieleo@gmail.com>
2018-06-06 18:40:58 +0300

    Add Administrator Class

--- app/models.py
+++ app/models.py
@@ -20,11 +20,28 @@ class USERS:
         self.dbconnect.commit()
 
     def login_user(self,email,password):
-        user = "SELECT * FROM Users WHERE Email = '{}' AND password = '{}' ".format(email,password)
+        user = "SELECT Email, password FROM Users WHERE Email = '{}' AND password = '{}' ".format(email,password)
         self.cursor.execute(user)
-        self.dbconnect.commit()    
+        user_data = self.cursor.fetchall()
+        user_list =[]
+        user_dictionary ={}
+        for i in user_data:
+            user_dictionary['Email'] = i[0]
+            user_dictionary['password'] = i[1] 
+            user_list.append(user_dictionary)   
+            user_dictionary = {}
+        return user_list
 
-    
+    def get_user_Id(self,Email):
+        user = "SELECT User_Id FROM Users WHERE Email = '{}'".format(Email)
+        self.cursor.execute(user)
+        user_id = self.cursor.fetchall()
+        for i in user_id:
+            USERID = i
+        return USERID    
+
+
+
 
 class Requests():
     def __init__(self):
@@ -32,7 +49,7 @@ class Requests():
         self.cursor = self.dbconnect.cursor()
 
     def create_request_table(self):
-        request_table_create = "CREATE TABLE Requests(Request_Id serial PRIMARY KEY, UserId INT REFERENCES Users(User_Id), RequestType varchar(100) NOT NULL, RequestDetails varchar(200) NOT NULL )"
+        request_table_create = "CREATE TABLE Requests(Request_Id serial PRIMARY KEY, UserId INT REFERENCES Users(User_Id), RequestType varchar(100) NOT NULL, RequestDetails varchar(200) NOT NULL, Status varchar(100) )"
         self.cursor.execute(request_table_create)
         self.dbconnect.commit()
 
@@ -55,6 +72,7 @@ class Requests():
             request_dictionary['userId'] = i[1]
             request_dictionary['requestType'] = i[2]
             request_dictionary['details'] = i[3]
+            request_dictionary['status'] = i[4]
             request_List.append(request_dictionary)
             request_dictionary ={}
         return request_List    
@@ -63,7 +81,19 @@ class Requests():
     def all_requests_for_specific_user(self,userId):
         requests = "SELECT * FROM Requests WHERE UserId = '{}' ".format(userId)
         self.cursor.execute(requests)
-        self.dbconnect.commit()          
+        self.dbconnect.commit()
+        request = self.cursor.fetchall()
+        request_dictionary = {}
+        request_list = []
+        for i in request:
+            request_dictionary['request_Id'] = i[0]
+            request_dictionary['userId'] = i[1]
+            request_dictionary['requestType'] = i[2]
+            request_dictionary['details'] = i[3]
+            request_dictionary['status'] = i[4]
+            request_list.append(request_dictionary)
+            request_dictionary ={}
+        return request_list         
     
     def specific_request(self, requestId):
         request = "SELECT * FROM Requests WHERE Request_Id = '{}'".format(requestId)
@@ -76,6 +106,7 @@ class Requests():
             request_dictionary['userId'] = i[1]
             request_dictionary['requestType'] = i[2]
             request_dictionary['details'] = i[3]
+            request_dictionary['status'] = i[4]
             request_List.append(request_dictionary)
             request_dictionary ={}
         return request_List
@@ -85,17 +116,64 @@ class Requests():
         self.cursor.execute(new_request)
         self.dbconnect.commit()
 
-        
-        
 
-        
+class Administrator():
+    def __init__(self):
+        self.dbconnect = psycopg2.connect(database="postgres", user = "postgres", password = "test123", host = "127.0.0.1", port = "5432")
+        self.cursor = self.dbconnect.cursor()      
 
+    def get_all_requests(self):
+        requests = "SELECT * FROM Requests"
+        self.cursor.execute(requests)
+        self.dbconnect.commit()
+        request = self.cursor.fetchall()
+        request_dictionary = {}
+        request_list = []
+        for i in request:
+            request_dictionary['request_Id'] = i[0]
+            request_dictionary['userId'] = i[1]
+            request_dictionary['requestType'] = i[2]
+            request_dictionary['details'] = i[3]
+            request_dictionary['status'] = i[4]
+            request_list.append(request_dictionary)
+            request_dictionary ={}
+        return request_list   
 
+    def view_all_users(self):
+        users = "SELECT * FROM Users"
+        self.cursor.execute(users)
+        self.dbconnect.commit()
+        all_users = self.cursor.fetchall()
+        all_user_dictionary = {}
+        all_user_list = []
+        for i in all_users:
+            all_user_dictionary['user_Id'] = i[0]
+            all_user_dictionary['FirstName'] = i[1]
+            all_user_dictionary['LastName'] = i[2]
+            all_user_dictionary['Email'] = i[3]
+            all_user_dictionary['password'] = i[4]
+            all_user_list.append(all_user_dictionary)
+            all_user_dictionary ={}
+        return all_user_list  
+
+    def approve_request(self,request_id):
+        request = "UPDATE Requests SET Status = '{}' WHERE Request_Id = '{}'".format('Approved', request_id)      
+        self.cursor.execute(request)
+        self.dbconnect.commit()
+
+    def disapprove_request(self,request_id):
+        request = "UPDATE Requests SET Status = '{}' WHERE Request_Id = '{}'".format('Disapproved', request_id)      
+        self.cursor.execute(request)
+        self.dbconnect.commit()
+
+    def resolve_request(self,request_id):
+        request = "UPDATE Requests SET Status = '{}' WHERE Request_Id = '{}'".format('Resolved', request_id)      
+        self.cursor.execute(request)
+        self.dbconnect.commit()
+
+   
 
     
-'''if __name__ == '__main__':
-    User_table = USERS()
-    Request_table = Requests() '''
 
     
 
