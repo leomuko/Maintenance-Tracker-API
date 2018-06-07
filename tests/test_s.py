449c68e431556a087bd63797f2d6cec61063fe3e
leomuko <kezzieleo@gmail.com>
2018-06-07 12:48:28 +0300

    create tests for user_Endpoints

---
+++ tests/test_s.py
@@ -0,0 +1,65 @@
+import unittest
+import json
+from app.user_Endpoints import app
+from flask import Flask
+
+
+
+class TestRequests(unittest.TestCase):
+ 
+    def test_user_signup(self):
+        self.client = app.test_client
+        response = self.client().post('/auth/signup',  content_type="application/json", data = json.dumps(dict(FirstName = 'katwere', LastName = 'Leo', Email ='kat@gmail',password ='password'))) 
+        self.assertEqual(response.status_code, 201)
+
+    def test_login_user(self):
+        self.client = app.test_client
+        login_data = {'Email':'kat@gmail', 'password':'password'}
+        response = self.client().post('/auth/login', content_type = "application/json", data = json.dumps(login_data))
+        self.assertEqual(response.status_code, 201)  
+
+        
+
+    def test_create_request(self):
+        self.client = app.test_client
+        user_request ={ 
+                        'RequestType': 'Pc shutdown',
+                         'Details': 'My pc cant turn back on'
+                       }    
+        response = self.client().post('/users/requests',  content_type="application/json",data = json.dumps(user_request))
+        self.assertEqual(response.status_code, 200)
+
+    def test_specific_user_requests(self):
+        self.client = app.test_client
+        user_data = {'Email':'what@gmail'}
+        response = self.client().get('api/v1/users/requests/9',  content_type="application/json",data = json.dumps(user_data))
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_modify_request(self):
+        self.client = app.test_client
+        user_request ={ 
+                        'requestType': 'Pc shutdown',
+                         'details': 'My pc cant turn back on'
+                       }    
+        response = self.client().put('/api/v1/users/requests/1', content_type = "application/json", data=json.dumps(user_request))
+        self.assertEqual(response.status_code, 200)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__=='__main__':
+    unittest.main()
