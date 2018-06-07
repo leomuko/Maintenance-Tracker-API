7fdc82dc1f3d4d8fb2562a72251f57ba10d618c3
leomuko <kezzieleo@gmail.com>
2018-06-07 23:21:50 +0300

    Modify user endpoints

--- app/user_Endpoints.py
+++ app/user_Endpoints.py
@@ -5,7 +5,7 @@ import jwt
 from functools import wraps
 from models import Requests
 import re
-
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 
@@ -41,18 +41,25 @@ def user_signup():
     Lastname = request.json.get('Lastname')
     Email = request.json.get('Email')
     password = request.json.get('password')
+    hashed_password = generate_password_hash(password)
     if not Firstname:
         return jsonify({'Message':'Please Enter FisrtName'})
+    elif not isinstance(Firstname, basestring) :
+        return jsonify({'message':'Firstname must be string'})    
     elif not Lastname:
         return jsonify({'Message':'Please Enter LastName'})
+    elif not isinstance(Lastname,basestring):
+        return jsonify({'message':'Lastname must be string'})      
     elif not Email:
         return jsonify({'Message':'Please Enter Email'})
     elif not re.match(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]", Email):
         return jsonify({'Message':'Enter valid Email'})
     elif not password:
         return jsonify({'Message':'Please Enter Password'})
+    elif len(password) < 4:
+        return jsonify({'message':'Enter stronger password'})  
     else:
-       User_table.create_new_user(Firstname, Lastname,Email,password)
+       User_table.create_new_user(Firstname, Lastname,Email,hashed_password)
 
     return jsonify({'Message': 'New User Created'}), 201
 
@@ -60,29 +67,43 @@ def user_signup():
 def user_login():
     Email = request.json.get('Email')
     password = request.json.get('password')
-    if not User_table.login_user(Email,password):
+    hashed_password = User_table.retrieve_user_password(Email)
+    if not check_password_hash(hashed_password,password):
+        return jsonify({'message':'Enter correct password'})
+    elif not User_table.login_user(Email,hashed_password):
         return jsonify({'message':'Please check your email or password'})
     else:
         token = jwt.encode({'Email':Email}, app.config['SECRET KEY'])   
         return jsonify({'token':token.decode('UTF-8')}),201
+             
 
 @app.route('/users/requests',methods=['POST'])
 @token_required
 def create_user_request(current_user):
     Email = request.json.get('Email')
-    
+    password = request.json.get('password')
     RequestType = request.json.get('RequestType')
     RequestDetails = request.json.get('Details')
+    
     if not Email:
         return jsonify({'Message':'Please Review your Email'})
     elif not re.match(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]", Email):
         return jsonify({'Message':'Please Review your Email'})
     elif Email != current_user['Email']:
-        return jsonify({'Message':'Enter valid Email'})    
+        return jsonify({'Message':'Enter valid Email'})
+    else: 
+        hashed_password = User_table.retrieve_user_password(Email)
+
+    if not check_password_hash(hashed_password,password): 
+        return jsonify({'message':'please enter valid password'})
     elif not RequestType:
         return jsonify({'message':'Please enter Request Type'})
+    elif not isinstance(RequestType,basestring):
+        return jsonify({'message':'RequestType must be string'})      
     elif not RequestDetails:
-        return jsonify({'message':'Please Enter Request Details'})    
+        return jsonify({'message':'Please Enter Request Details'})
+    elif not isinstance(RequestDetails,basestring):
+        return jsonify({'message':'RequestDetails must be string'})      
     else:
         Userid = User_table.retrieve_user_Id(Email)
         Request_table.create_new_user_request(Userid,RequestType,RequestDetails)
@@ -108,7 +129,7 @@ def view_specific_request(current_user,Request_id):
     Userid = User_table.retrieve_user_Id(Email)
     spec_requests = Request_table.specific_request(Request_id,Userid)
     if len(spec_requests) == 0:
-        return jsonify({'message':' Please Enter valid request id'})
+        return jsonify({'message':'You do not have a request by that id. Please review your requests and enter valid request Id'})
     else:    
         return jsonify(spec_requests)
    
@@ -119,17 +140,30 @@ def modify_user_request(current_user,Request_id):
     R_details = request.json.get('Details')
     inputEmail = request.json.get('Email')
     Email = current_user['Email']
+    password = request.json.get('password')
     Userid = User_table.retrieve_user_Id(Email)
-    if not inputEmail:
+    request_check = Request_table.specific_request(Request_id, Userid)
+    if len(request_check) == 0:
+        return jsonify({'message':'You do not have a request by that id. Please review your requests and enter valid request Id'})
+    elif not inputEmail:
         return jsonify({'Message':'Please Enter your Email'})
     elif not re.match(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]", inputEmail):
         return jsonify({'Message':'Please Review your Email'})
     elif inputEmail != current_user['Email']:
         return jsonify({'Message':'Enter valid Email'})
+    else:
+        hashed_password = User_table.retrieve_user_password(Email)
+    
+    if not check_password_hash(hashed_password,password): 
+        return jsonify({'message':'please enter valid password'})
     elif not R_type:
         return jsonify({'message':'Enter request type'})
+    elif not isinstance(R_type,basestring):
+        return jsonify({'message':'Request Type must be string'})      
     elif not R_details:
         return jsonify({'message':'Enter request details'})
+    elif not isinstance(R_details,basestring):
+        return jsonify({'message':'Request Details must be string'})     
     elif Request_table.check_if_request_exists(Userid, Request_id) is False:
         return jsonify({'message':'You do not have access to the request'})   
     else:   
